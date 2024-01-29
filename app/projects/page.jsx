f53dead375f855d664ac94a66dc10a8bee556243
hothidhruv <hothidhruv@gmail.com>
2024-01-29 23:43:44 +0530

    23-01

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+'use client';
+import React, {useState} from 'react';
+import { Tabs } from '@/components/ui/tabs';
+import ProjectCard from '@/components/ProjectCard';
+
+const projectData = [
+  {
+    image: "/work/3.png",
+    category: "Next js",
+    name: "xyzd wwf",
+    description:
+      "Work  Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "Javascript",
+    name: "xyzd wwf",
+    description:
+      "Work  Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "react js",
+    name: "xyzd wwf",
+    description:
+      "Work  Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "react js",
+    name: "xyzd wwf",
+    description:
+      "Work  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius laboriosam, excepturi rerum commodi ipsam",
+    link: "/",
+    github: "/",
+  },
+];
+console.log(projectData);
+
 
 const Projects = () => {
   return (
-    <div>Projects Page</div>
+    <section className='min-h-screen pt-12'>Projects Page</section>
   );
 };
 
-export default Projects
+export default Projects;
