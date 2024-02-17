8b710edf8f51f32cd7d1d35ad3f806df3bd72a32
hothidhruv <hothidhruv@gmail.com>
2024-02-17 13:30:47 +0530

    done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,7 @@
 import { MailIcon, HomeIcon, PhoneCall, Component } from "lucide-react";
 
 // Components
-import Form from "@/components/form";
+import Form from "../../components/ui/form";
 
 const Contact = () => {
   return (
